ff5e89b600f8e2d3be71be5327fd1aea4bf3dd0b
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-01 01:10:49 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,7 +1,7 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I'm StudyBudAI 📚 </h1>
-    <p>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I'm StudyBud AI 📚 </h1>
+    <p className="text-base/loose">
       StudyBudAI can help students by using AI as a tool to retrieve relevant information, organize resources more effeciently and save valuable time in the learning process. 
       Students can also use StudyBudAI to get subjects explained in ways that will help them better understand it.
     </p>
