022462fad7530a083c7fc519820f67d5323e7a10
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-01 19:05:04 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,9 +1,9 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I'm StudyBud AI 📚</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 I'm Ryan-AI</h1>
     <p className="text-base/loose">
-      StudyBud AI helps students use AI as a tool to retrieve relevant information, organize resources more effeciently and save valuable time in the learning process. 
-      Students can also use StudyBud AI to get subjects explained in ways that will help them better understand it.
+      Don't worry, I'm WAY smarter than Ryan(The human version) that built me 😉  
+      Ask me questions or chat with me in the text box below.
     </p>
     <p className="text-lg font-bold mb-2">You can try some examples below ⬇</p>
   </div>
