a44d40b0838177c871b402cb67dc19329e4fee39
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-04-30 23:08:10 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,12 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I'm StudyBuddy AI 📚 </h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      StudyBuddyAI utilizes AI-powered algorithms that retrieves information, makes traditional note-taking more efficient and organizes documents, files, and resources      
+      saving students valuable time in their research and learning process.
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
+   
     </p>
   </div>
 );
