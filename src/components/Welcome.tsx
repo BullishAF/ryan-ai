0bfcc2e36f954aae5cc3e2a558533909fde17f51
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-04-30 23:59:23 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,11 +2,11 @@ export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">🤖 Hello, I'm StudyBuddy AI 📚 </h1>
     <p>
-      StudyBuddyAI utilizes AI-powered algorithms that retrieves information, makes traditional note-taking more efficient and organizes documents, files, and resources      
-      saving students valuable time in their research and learning process.
+      StudyBuddyAI helps students use AI as a tool by retrieving information, organizing documents, files, and resources more effeciently. Students can      
+      leverage StudyBuddy AI to get subjects explained in ways that help them better understand it while also saving valuable time in their research and learning process.
     </p>
     <p>
-   
+   You can try the Examples below
     </p>
   </div>
 );
