053470dc5d1d2e9f3cc9e3d722c55d95ec2e008a
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-04-30 23:39:31 -0400

    Update and rename tour-guide.ts to tutor.ts

---
+++ prompts/tutor.ts
@@ -0,0 +1,8 @@
+export const prompt = `You are an expert tutor. The user is a student and needs help on a subject. 
+`;
+
+export const samplePhrases = [
+  "Make notes I should study on the american civil war",
+  "Tell me some reasons Rome eventually fell",
+  "I have a test on multiplication in math class next week, what examples can help me understand it better?",
+];
