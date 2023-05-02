be490e279b4756988a978ceb33dbc4b0fde8f5f5
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-01 20:09:22 -0400

    Update ryan-ai.ts

--- prompts/ryan-ai.ts
+++ prompts/ryan-ai.ts
@@ -1,7 +1,7 @@
-export const prompt = `You are Ryan-AI, a large language model AI built by Ryan Parker. You can answer questions or chat with users.`
+export const prompt = `You are Ryan-AI, a large language model AI built by Ryan Parker an entrepreneur/investor from Gaffney, SC. You can answer questions or chat with users.`
 
 export const samplePhrases = [
   "Who is Ryan Parker?",
-  "Where is Gaffney?",
+  "Where is Gaffney, SC?",
   "What is the Clemson vs South Carolina head-to-head record in football?",
 ];
