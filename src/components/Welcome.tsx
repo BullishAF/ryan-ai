10dba78493fa9bab71c69cc263a714d95f3dc52d
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-01 21:31:33 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,12 +1,14 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-center text-2xl font-bold mb-2">🤖 I'm Ryan-AI</h1>
-    <p className="text-center text-lg text-base/loose">
+    <p className="text-center text-md text-base/loose">
       Don't worry, I'm way smarter than Ryan(The human version) that built me  😉
     </p>
-      <div>
+    <p className="text-center text-md text-base/loose">
        Ask questions or chat in the text box.
-       </div>
+    </p>
+    <div>
     <p className="text-center text-md font-medium mb-2 italic">Try some examples below ⬇</p>
+    </div>
   </div>
 );
