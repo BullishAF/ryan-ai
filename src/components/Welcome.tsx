cbfcb6885d4c5efccf445596ee5636f089833bfc
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-03 12:55:08 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,5 +1,5 @@
 export const Welcome: React.FC = () => (
-<div class="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-400 from-green-500 p-10">
+<div class="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-400 from-green-500 p-1">
  
   <div class="w-max">
     <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold">Hello I'm AIMindz</h1>
