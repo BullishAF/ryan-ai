a96ad5fd591077cb3af62e9ac329b4117ecb1b10
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-03 13:59:24 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -2,5 +2,6 @@ export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-200 border-2 rounded-lg px-8 py-5 mr-20 w-full"> 
   <div class="w-max">
   <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-transparent bg-clip-text bg-gradient-to-r from-slate-400 via-green-400 to-emerald-600">AIMindz</h1>
-    </div>
+  </div>
+</div>
 );
