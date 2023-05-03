5e780a0353473693548103c31047508bc86cd8c4
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-03 12:50:35 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,24 +1,9 @@
 export const Welcome: React.FC = () => (
-  <header className="mb-16 group">
-  <h1 className="mb-1 font-mono text-4xl text-gray-900 md:text-6xl">
-    Hi, I'm <br className="block md:hidden" />
-    <span
-      className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-green-400 will-change-transform"
-    >
-      AIMindz 👋
-    </span>
-    </h1>
-</header>
-<div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-center text-2xl font-bold mb-2">🤖 I'm Ryan-AI</h1>
-    <p className="text-center text-md text-base/loose">
-      Don't worry, I'm way smarter than Ryan(The human version) that built me  😉
-    </p>
-    <p className="text-center text-md text-base/loose">
-       Ask questions or chat in the text box.
-    </p>
-    <div>
-    <p className="text-center text-md font-medium mb-2 italic">Try some examples below ⬇</p>
-    </div>
+<div class="flex min-h-screen items-center justify-center bg-gradient-to-tr to-blue-400 from-green-500 p-10">
+ 
+  <div class="w-max">
+    <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-white font-bold">Hello I'm AIMindz</h1>
   </div>
-);
+   
+  </div>
+</div>
