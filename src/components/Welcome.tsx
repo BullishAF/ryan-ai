99a2085048a404fbd31d44d1386384a6b657d7bd
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-03 14:31:06 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,7 +1,7 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-200 border-2 rounded-lg px-8 py-5 mr-20 w-full"> 
   <div class="w-max">
-  <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-green-400">AIMindz</h1>
+  <h1 class="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-5xl text-green-400 text-center">Hello, I'm AIMindz</h1>
   </div>
 </div>
 );
