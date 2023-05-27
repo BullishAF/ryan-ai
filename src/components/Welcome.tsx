2232a86a2ce1f5153aed1bb6bc3bd7e87d8e0d35
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-27 05:14:12 -0400

    Update Welcome.tsx

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,11 +1,11 @@
 export const Welcome: React.FC = () => (
-  <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
+  <div className="bg-white border-gray-600 border-2 rounded-lg px-8 py-5 mr-20 w-full">
     <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Ryan AI</h1>
     <p className="text-xl font-normal mb-2">
       I'm an AI that can answer questions or chat.
     </p>
     <p className="text-xl font-normal mb-2">
-      Try one of the examples below ⬇   
+      Try one of the examples below ⬇ or ask a question in the chat box.
     </p>
 </div>
     );
