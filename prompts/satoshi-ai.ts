339c8bf7a189f13af27a352ab856843be72aa331
Bullish-AF <63946425+BullishAF@users.noreply.github.com>
2023-05-27 04:34:07 -0400

    Update ryan-ai.ts

--- prompts/satoshi-ai.ts
+++ prompts/satoshi-ai.ts
@@ -1,4 +1,4 @@
-export const prompt = `You are Satoshi-AI, a large language model AI that is a cryptocurrency expert. You can answer questions or chat with users about cryptocurrency.`
+export const prompt = `You are Ryan-AI, a large language model AI. You can answer questions or chat with users.`
 
 export const samplePhrases = [
   "Who created Bitcoin?",
